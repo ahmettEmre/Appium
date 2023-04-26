17e4ed633d1310f71678821b2601a50566050299
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-04-26 13:02:12 +0200

    Appium

--- src/test/java/ArabamAppTest.java
+++ src/test/java/ArabamAppTest.java
@@ -1,2 +1,38 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 public class ArabamAppTest {
+
+    AndroidDriver<AndroidElement> driver;
+    final String cihazAdi="PIXEL2";
+    final String platformIsmi="Android";
+    final String version="10.0";
+    final String automation="UiAutomator2";
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, cihazAdi);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, automation);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, platformIsmi);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, version);
+        capabilities.setCapability("apppackage","com.dogan.arabam");//hangi uygulama üzerinde calisacagiz
+        capabilities.setCapability("appActivity","com.dogan.arabam.presentation.feature.home.HomeActivity");//uygulamada hangi sayfada baslayacagimiz yer
+
+        driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+    @Test
+    public void arabamTest(){
+    }
 }
