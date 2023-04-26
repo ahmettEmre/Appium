c65cb733ddf095818d57fde310c5c04cfd4bc2ab
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-04-26 12:03:15 +0200

    Appium

--- src/test/java/denemeTesti.java
+++ src/test/java/denemeTesti.java
@@ -1,16 +1,22 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class denemeTesti {
 
 
-    AndroidDriver<MobileElement> driver;
+    AndroidDriver<AndroidElement> driver;
 
     @Test
-    public void deneme1(){
+    public void deneme1() throws MalformedURLException {
 
         DesiredCapabilities capabilities=new DesiredCapabilities();
 
@@ -21,6 +27,28 @@ public class denemeTesti {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\ahmet\\IdeaProjects\\APPIUM-T108\\Apps\\Calculator_8.4 (503542421)_Apkpure.apk");
 
+        driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    //@Test
+    public void carpma(){
+
+        driver.activateApp("com.google.android.calculator");
+        driver.findElementByAccessibilityId("8").click();
+        driver.findElementByAccessibilityId("0").click();
+        driver.findElementByAccessibilityId("multiply").click();
+        driver.findElementByAccessibilityId("2").click();
+        driver.findElementByAccessibilityId("0").click();
+        String result= driver.findElementById("com.google.android.calculator:id/result_preview").getText();
+        System.out.println(result);
+        Assert.assertEquals(Integer.parseInt(result),1600);
+    }
+
+
+
+    @Test
+    public void toplama(){
 
     }
 }
